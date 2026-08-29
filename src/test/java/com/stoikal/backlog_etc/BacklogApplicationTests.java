3c8e8d7124b88d4afb65e8192db8ed2d3dc62eca
Laksito Pamilih <laksito@protonmail.com>
2026-08-29 20:49:38 +0700

    init

---
+++ src/test/java/com/stoikal/backlog_etc/BacklogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stoikal.backlog_etc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BacklogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
