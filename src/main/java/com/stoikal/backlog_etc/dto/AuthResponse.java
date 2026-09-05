6b32d8b2abd77d96b638a7461b6b6afc343cab54
Laksito Pamilih <laksito@protonmail.com>
2026-09-05 23:17:12 +0700

    feat: auth dto

---
+++ src/main/java/com/stoikal/backlog_etc/dto/AuthResponse.java
@@ -0,0 +1,3 @@
+package com.stoikal.backlog_etc.dto;
+
+public record AuthResponse(String accessToken) {}
---
+++ src/main/java/com/stoikal/backlog_etc/dto/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.stoikal.backlog_etc.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @NotBlank @Email String email,
+        @NotBlank String password
+) {}
