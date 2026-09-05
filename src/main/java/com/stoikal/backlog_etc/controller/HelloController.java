dcd682b758fd54230098cadc1e47f50457b2df6b
Laksito Pamilih <laksito@protonmail.com>
2026-09-05 14:49:43 +0700

    purge

--- src/main/java/com/stoikal/backlog_etc/controller/HelloController.java
+++ src/main/java/com/stoikal/backlog_etc/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/api/hello")
     public String hello() {
-        return "Hello, Authenticated User!";
+        return "Hello, world!";
     }
 }
--- src/main/java/com/stoikal/backlog_etc/repository/GameRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stoikal.backlog_etc.repository;
-
-import com.stoikal.backlog_etc.entity.Game;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GameRepository extends JpaRepository<Game, Integer> {
-}
